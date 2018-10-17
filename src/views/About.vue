7b6502630989c38b917e5cc94875d85f99f3e215
ShayanSh <shayan.shsh@yahoo.com>
2018-10-17 22:31:05 +0200

    working "MyFirstView.vue"

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <MyFirstVue welcomeMsg="this works!"/>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <MyFirstView welcome-msg="Your welcome message!"/>
+    </div>
 </template>
+
 <script>
     import MyFirstView from "./MyFirstView";
-
     export default {
-        components: { MyFirstView}
+        components: {MyFirstView}
     }
 </script>
