63416f520a00e7e423fc6c4ca102db9f4954ffbb
ShayanSh <shayan.shsh@yahoo.com>
2018-10-17 19:52:56 +0200

    initial commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <MyFirstVue welcomeMsg="this works!"/>
   </div>
 </template>
+<script>
+    import MyFirstView from "./MyFirstView";
+
+    export default {
+        components: { MyFirstView}
+    }
+</script>
---
+++ src/views/MyFirstView.vue
@@ -0,0 +1,20 @@
+<template>
+    <p class="green-background"> {{welcomeMsg}}</p>
+</template>
+
+<script>
+    export default {
+        name: "MyFirstView",
+        props:{
+            welcomeMsg : String
+        }
+    }
+</script>
+
+<style scoped>
+
+    .green-background{
+        background: #4fc08d;
+    }
+
+</style>
