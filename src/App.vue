93dae2821e312a17ab24e9f30653646323b378a9
Shayan <Shayan.shsh@yahoo.com>
2018-10-20 13:04:36 +0200

    simple version of navigation bar done!

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>  |
-      <router-link to="/MyFirstView">First View</router-link>
+      <ul class="navmenu">
+        <li><router-link to="/">Home</router-link> </li>
+        <li><router-link to="/about">About</router-link> </li>
+        <li><router-link to="/MyFirstView">First View</router-link></li>
+        <li><router-link to="/MyFirstView">First View</router-link></li>
+      </ul>
     </div>
     <router-view/>
   </div>
@@ -23,10 +26,49 @@
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #637688;
 }
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+ul.navmenu{
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #2c3e50;
+  display: flex;
+  flex-direction: row; /* keep the contents in a row */
+  flex-wrap: wrap; /* bring contents to the next line if necessary */
+  justify-content: center; /* keep the contents in the middle */
+}
+
+
+ul.navmenu li{
+  float: left;
+}
+ul.navmenu li a{
+  color: white;
+  display: block;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+ul.navmenu li a:hover{
+  background-color: darkslategray;
+}
+
+ul.navmenu li a:active{
+  background-color: darkslategray;
+}
+
+ul.navmenu .active{
+  background-color: darkgray;
 }
 </style>
